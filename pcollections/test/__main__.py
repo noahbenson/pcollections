a61b3a4bccbb9b6a372671e9914f8c03096d9d2d
Noah Benson <nben@uw.edu>
2023-01-31 10:52:59 -0800

    adding main file so that test package can be called directly

---
+++ pcollections/test/__main__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+################################################################################
+# pcollections/test/__main__.py
+# Imports the test package so that tests can be run from the command line by
+# calling the test module directly.
+# By Noah C. Benson
+
+import unittest
+
+from pcollections.test import *
+
+unittest.main()
+
+
